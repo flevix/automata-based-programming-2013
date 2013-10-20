26df9a55955b36e3a104ef342e5e0adc2c3f9fa3
flevix.ru@gmail.com <flevix.ru@gmail.com>
2013-10-20 05:26:38 +0400

    Minimization init

---
+++ lab2/Minimization.java
@@ -0,0 +1,68 @@
+import java.util.*;
+import java.io.*;
+
+/**
+ * Created with VIM.
+ * User: flevix
+*/
+
+public class Minimization {
+    FastScanner in;
+    PrintWriter out;
+
+    public void solve() throws IOException {
+    }
+
+    public void run() {
+        try {
+            in = new FastScanner();
+            out = new PrintWriter(System.out);
+
+            solve();
+
+            out.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Minimization().run();
+    }
+
+    class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+
+        FastScanner() {
+            try {
+                br = new BufferedReader(new InputStreamReader(System.in));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        FastScanner(File f) {
+            try {
+                br = new BufferedReader(new FileReader(f));
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+        }
+
+        String next() {
+            while (st == null || !st.hasMoreTokens()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+    }
+}
