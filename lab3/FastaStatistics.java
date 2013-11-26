2be90e48e33e4c7edb83c0f2c7142397c4b4e069
flevix.ru@gmail.com <flevix.ru@gmail.com>
2013-11-27 02:38:13 +0400

    Fasta-statistics done

--- lab3/FastaStatistics.java
+++ lab3/FastaStatistics.java
@@ -5,34 +5,49 @@ import java.util.Collections;
 
 public class FastaStatistics {
 
-    int statistic(ArrayList<Integer> sizes, final double mean,
-                                            final int full_length) {
-        return full_length;
+    int statistic(final ArrayList<Integer> lengths, final double mean,
+                                                  final int full_length) {
+        final double required_mean = full_length * mean;
+        double curr_mean = 0;
+        int length = lengths.get(lengths.size() - 1);
+        for (int i = 0; i < lengths.size(); i++) {
+            curr_mean += lengths.get(i);
+            if (curr_mean >= required_mean) {
+                length = lengths.get(i);
+                break;
+            }
+        }
+        return length;
     }
 
-    private void solve(FastScanner in, PrintWriter out) {
-        ArrayList<Integer> sizes = new ArrayList<>();
+    private void solve(final FastScanner in, final PrintWriter out) {
+        ArrayList<Integer> lengths = new ArrayList<>();
         String line = in.nextLine();
 
         int current_length = 0;
         int full_length = 0;
         while ((line = in.nextLine()) != null) {
             if (line.charAt(0) == '>') {
-                sizes.add(current_length);
+                lengths.add(current_length);
                 full_length += current_length;
                 current_length = 0;
             } else {
                 current_length += line.length();
             }
         }
-        sizes.add(current_length);
+        lengths.add(current_length);
         full_length += current_length;
-        
-        out.println(statistic(sizes, 0.5D, full_length));
-        out.println(statistic(sizes, 0.9D, full_length));
+
+        Collections.sort(lengths, Collections.reverseOrder());
+
+        out.println(statistic(lengths, 0.5D, full_length));
+        out.println(statistic(lengths, 0.9D, full_length));
+        out.println(lengths.get(lengths.size() - 1));
+        out.println(lengths.get(0));
+        out.println(((double) full_length / lengths.size()));
     }
 
-    public void run(String fileName) {
+    public void run(final String fileName) {
         try {
             FastScanner in = new FastScanner(new File(fileName + ".fasta"));
             PrintWriter out = new PrintWriter(new File(fileName + ".out"));
