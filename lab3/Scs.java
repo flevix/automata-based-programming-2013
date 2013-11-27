3edfb5ddd5cb32b7cf249294dc24de3d788f19a9
flevix.ru@gmail.com <flevix.ru@gmail.com>
2013-11-28 00:02:40 +0400

    overlaps matrix calculate

--- lab3/Scs.java
+++ lab3/Scs.java
@@ -1,10 +1,58 @@
 import java.io.*;
 import java.util.StringTokenizer;
 import java.lang.Math;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
 
 public class Scs {
 
+    private Set<String> getInputSet(final FastScanner in) {
+        Set<String> strings = new HashSet<>();
+        int n = in.nextInt();
+        for (int i = 0; i < n; i++) {
+            strings.add(in.next());
+        }
+        return strings;
+    }
+
+    private List<String> getUniqueList(final Set<String> data) {
+        Set<String> strings = new HashSet<>();
+        for (String a : data) {
+            boolean ok = true;
+            for (String b : data) {
+                if (b.contains(a) && !b.equals(a)) {
+                    ok = false;
+                }
+            }
+            if (ok) {
+                strings.add(a);
+            }
+        }
+        return new ArrayList<String>(strings);
+    }
+
+    private int[][] getOverlaps(final int n, final List<String> data) {
+        int[][] overlaps = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i != j) {
+                    overlaps[i][j] = overlap(data.get(i), data.get(j));
+                }
+            }
+        }
+        return overlaps;
+    }
+
     private void solve(final FastScanner in, final PrintWriter out) {
+        List<String> list = getUniqueList(getInputSet(in));
+        System.out.println(Arrays.toString(list.toArray()));//-
+        final int n = list.size();
+        int[][] overlaps = getOverlaps(n, list);
+        for (int[] a : overlaps)//-
+            System.out.println(Arrays.toString(a));//-
     }
     
     private static int overlap(final String s1, final String s2) {
